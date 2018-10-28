81c9440bd5f80d7a56dd2d01590a582e5ffe7f8f
neogong_Mac <xy_Gong@foxmail.com>
2018-10-27 19:16:51 -0500

    update filtered_evaluate

--- bdd_data/filtered_evaluate.py
+++ bdd_data/filtered_evaluate.py
@@ -15,10 +15,10 @@ from tqdm import tqdm
 def parse_args():
     """Use argparse to get command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('name_list', type=str, help='path to the name list')
-    parser.add_argument('task', choices=['seg', 'det', 'drivable'])
-    parser.add_argument('gt', help='path to ground truth')
-    parser.add_argument('result', help='path to results to be evaluated')
+    parser.add_argument('--name_list', type=str, help='path to the name list')
+    parser.add_argument('--task', choices=['seg', 'det', 'drivable'])
+    parser.add_argument('--gt', help='path to ground truth')
+    parser.add_argument('--result', help='path to results to be evaluated')
     args = parser.parse_args()
 
     return args
